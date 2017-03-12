5c864fc8efd1d27fc82f656cce230ed81bb274b9
Jackson Inchalik <jinchalik@half-life.local>
2017-03-12 18:27:40 -0400

    Finished through chapter 12, serial number not working?

---
+++ Honepwner/ItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCell.swift
+//  Honepwner
+//
+//  Created by Jackson Inchalik on 3/12/17.
+//  Copyright © 2017 Jackson Inchalik. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        nameLabel.adjustsFontForContentSizeCategory = true
+        serialNumberLabel.adjustsFontForContentSizeCategory = true
+        valueLabel.adjustsFontForContentSizeCategory = true
+    }
+}
